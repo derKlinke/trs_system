df20c4dae21484ff62e33e779df991ab8d36dd14
Fabian Klinke <fabian.s.klinke@icloud.com>
2024-12-31 15:07:26 +0100

    removed TextFieldStyle
    Signed-off-by: Fabian Klinke <fabian.s.klinke@icloud.com>

--- sources/TRSSystem/styles/ButtonStyles.swift
+++ sources/TRSSystem/styles/ButtonStyles.swift
@@ -34,34 +34,10 @@ public struct TRSButtonStyle: ButtonStyle {
     }
 }
 
-// MARK: - TRSTextFieldStyle
-// FIXME: we need to find a concurrency safe way to do this
-// @MainActor
-// public struct TRSTextFieldStyle: TextFieldStyle {
-//     @EnvironmentObject private var themeManager: ThemeManager
-
-//     public init() {}
-
-//     public func _body(configuration: TextField<Self._Label>) -> some View {
-//         // Wrap the body content in a MainActor.run block
-//         let backgroundColor = Color(themeManager.color(for: .secondaryBackground).color)
-//         let textColor = Color(themeManager.color(for: .text).color)
-
-//         return configuration
-//             .textFieldStyle(PlainTextFieldStyle())
-//             .padding(.horizontal, .small)
-//             .frame(minHeight: .large)
-//             .background(backgroundColor)
-//             .foregroundColor(textColor)
-//             .font(trs: .mono)
-//             .borderClip(.tiny)
-//     }
-// }
-
 // MARK: - TRSToggleStyle
 public struct TRSToggleStyle: ToggleStyle {
     @EnvironmentObject private var themeManager: ThemeManager
-    @State private var isPressed = false // Make sure to mark as private for encapsulation
+    @State private var isPressed = false
 
     public init() {}
 
@@ -88,16 +64,14 @@ public struct TRSToggleStyle: ToggleStyle {
             .animation(.spring(duration: 0.1), value: isPressed)
             .gesture(DragGesture(minimumDistance: 0)
                 .onChanged { _ in
-                    // Triggered when the gesture starts
                     if !isPressed {
                         isPressed = true
                     }
                 }
                 .onEnded { _ in
-                    // Triggered when the gesture ends
                     if isPressed {
                         isPressed = false
-                        configuration.isOn.toggle() // Toggle the state on release
+                        configuration.isOn.toggle()
                     }
                 })
 
