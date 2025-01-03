26a58c2e05a2b0c6aca6da20b666f9e52735e039
Fabian Klinke <fabian.s.klinke@icloud.com>
2025-01-03 15:19:54 +0100

    renamed package
    Signed-off-by: Fabian Klinke <fabian.s.klinke@icloud.com>

--- Package.swift
+++ Package.swift
@@ -1,31 +1,25 @@
 // swift-tools-version: 6.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+// swift-format-ignore-file
 
 import PackageDescription
 
-let package = Package(name: "TRSSystem",
-                      platforms: [
-                          .macOS(.v14),
-                      ],
-                      products: [
-                          // Products define the executables and libraries a package produces, making them
-                          // visible to other packages.
-                          .library(name: "TRSSystem",
-                                   targets: ["TRSSystem"]),
-                      ],
-                      dependencies: [
-                      ],
-                      targets: [
-                          // Targets are the basic building blocks of a package, defining a module or a test
-                          // suite.
-                          // Targets can depend on other targets in this package and products from
-                          // dependencies.
-                          .target(name: "TRSSystem",
-                                  resources: [
-                                      .process("sound/audio_files"),
-                                      .process("styles/fonts/custom_fonts"),
-                                  ]),
+let package = Package(name: "trs_system",
+    platforms: [
+        .macOS(.v14),
+    ],
+    products: [
+        .library(name: "trs_system",
+                targets: ["trs_system"]),
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .target(name: "trs_system",
+                resources: [
+                    .process("sound/audio_files"),
+                    .process("styles/fonts/custom_fonts"),
+                ]),
 
-                          .testTarget(name: "TRSSystem-snapshot",
-                                      dependencies: ["TRSSystem"]),
-                      ])
+        .testTarget(name: "trs_system-snapshot",
+                    dependencies: ["trs_system"]),
+    ])
