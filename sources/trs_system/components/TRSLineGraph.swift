026ef726053ef91730e1b0973d0355703f7c1ea6
Fabian Klinke <fabian.s.klinke@icloud.com>
2025-01-05 20:27:25 +0100

    made color of line in graph theme dependent
    Signed-off-by: Fabian Klinke <fabian.s.klinke@icloud.com>

--- sources/trs_system/components/TRSLineGraph.swift
+++ sources/trs_system/components/TRSLineGraph.swift
@@ -3,6 +3,7 @@ import SwiftUI
 
 public struct TRSLineGraph: View {
     private var dataPoints: [Float]
+    @EnvironmentObject private var themeManager: ThemeManager
 
     public init(dataPoints: [Float]) {
         self.dataPoints = dataPoints
@@ -12,5 +13,6 @@ public struct TRSLineGraph: View {
         Chart(Array(dataPoints.enumerated()), id: \.offset) { index, value in
             LineMark(x: .value("Index", index), y: .value("Value", value))
         }
+        .foregroundColor(themeManager.color(for: .secondaryText).color)
     }
 }
